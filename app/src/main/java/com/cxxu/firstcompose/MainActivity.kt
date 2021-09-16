03acae13a421c58d0723b0b2e7b8e11f2598e4f4
cxxu <838808930@qq.com>
2021-09-16 17:35:24 +0800

    add MessageCard3

--- app/src/main/java/com/cxxu/firstcompose/MainActivity.kt
+++ app/src/main/java/com/cxxu/firstcompose/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -81,30 +84,67 @@ fun MessageCard1Pre(/*empty parameter for previewFunction*/) {
 /*接下来我们设计/使用一些辅助排版的composable function*/
 @Composable
 fun MessageCard2(msg: Message) {
+    /*定义第一行的内容布局*/
 //    单一接口函数,神略圆括号花括号的lambda对象作为参数
     Row {
-/*首先放入一个image*/
+/*首先放入一个image(作为本行的第一列内容)*/
         Image(
             painter = painterResource(R.drawable.painter1),
             /*注意引用图片资源是R.drawable.xxx;而不像字符资源是R.id.xxx*/
             contentDescription = "Contact profile picture"
         )
+        /*放入一个列元素*/
         Column {
             /*注意,这个lambda对象内部的语句是两个函数调用(作为"表达式"主体)(而不是用逗号隔开的参数*/
+            /*列元素的第1~3行内容分别如下定义*/
             Text(text = msg.author)
             Text(text = msg.body)
+            Text(text = "testLine3")
         }
     }
 
 }
 
-@Preview
+//@Preview
 @Composable
 fun MessageCard2Pre() {
     MessageCard2(
         msg = Message("MessageCard2", "show!")
     )
 }
+
+@Composable
+fun MessageCard3(msg: Message) {
+    // Add padding around our message
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.painter1),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                // Set image size to 40 dp
+                .size(40.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+        )
+
+        // Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(text = msg.author)
+            // Add a vertical space between the author and message texts
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.body)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MessageCard3Pre() {
+    MessageCard3(msg = Message("MessageCard3", "isn't it beautiful?"))
+}
+
 //@Composable
 //fun Greeting(name: String) {
 //    Text(text = "Hello $name!")
